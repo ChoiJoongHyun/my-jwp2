9ba6ca369af2868caa564a11e290d32544a1c11e
joonghyun <joonghyun0117@gmail.com>
2016-12-08 20:24:18 +0900

    commit

--- src/main/java/slipp/model/Answer.java
+++ src/main/java/slipp/model/Answer.java
@@ -11,22 +11,29 @@ import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Entity
 public class Answer {
 	@Id
 	@GeneratedValue
+	@JsonProperty
 	private Long id;
 	
 	@ManyToOne
 	@JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_writer"))
+	@JsonProperty
 	private User writer;
 	
 	@ManyToOne
 	@JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_to_question"))
+	@JsonProperty
 	private Question question;
 	
 	@Lob
+	@JsonProperty
 	private String contents;
+	
 	
 	private LocalDateTime createDate;
 	
@@ -87,5 +94,9 @@ public class Answer {
 	public String toString() {
 		return "Answer [id=" + id + ", writer=" + writer + ", contents=" + contents + ", createDate=" + createDate
 				+ "]";
+	}
+
+	public boolean isSameWriter(User loginUser) {
+		return loginUser.equals(this.writer);
 	}
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,73 @@
+$(".answer-write input[type='submit']").click(addAnswer);
+
+function addAnswer(e) {
+	console.log('click answer btn');
+	e.preventDefault();
+
+	var url = $(".answer-write").attr("action");
+	console.log("url : " + url);
+
+	var queryString = $(".answer-write").serialize();
+	console.log("queryString : " + queryString)
+
+	$.ajax({
+		url : url,
+		type : 'post',
+		data : queryString,
+		dataType : 'json',
+		error : function() {
+			console.log("fail!");
+		},
+		success : function(data) {
+			console.log(data);
+			console.log(data.writer.userId);
+
+			var answerTemplate = $("#answerTemplate").html();
+			var template = answerTemplate.format(data.writer.userId,
+					data.formattedCreateDate, data.contents, data.question.id,
+					data.id);
+			$(".qna-comment-slipp-articles").prepend(template);
+			$("textarea[name=contents]").val("");
+		}
+
+	})
+}
+
+$(".form-answer-delete button[type='submit']").click(deleteAnswer);
+function deleteAnswer(e) {
+	console.log('click deleteAnswer btn');
+	e.preventDefault();
+
+	var url = $(".form-answer-delete").attr("action");
+	console.log("url : " + url);
+
+	var queryString = $(".form-answer-delete").serialize();
+	console.log("queryString : " + queryString)
+
+	var deleteBtn = $(this);
+	$.ajax({
+		url : url,
+		type : 'delete',
+		dataType : 'json',
+		error : function() {
+			console.log("fail!");
+		},
+		success : function(data) {
+			console.log(data);
+			
+			if(data.valid) {
+				deleteBtn.closest("article").remove();
+			} else {
+				alert("failfail");
+			}
+		}
+
+	})
+}
+
 String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+	var args = arguments;
+	return this.replace(/{(\d+)}/g, function(match, number) {
+		return typeof args[number] != 'undefined' ? args[number] : match;
+	});
 };
